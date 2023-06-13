72a481e231b229b9c97b0f0f35d589896cc3b0da
ChrisTitusTech <dfm.titus@gmail.com>
2023-06-13 09:45:17 -0500

    initial

---
+++ README
@@ -0,0 +1,7 @@
+# Personal NIX OS Configuration
+
+This configuration.nix is not meant to be used, but instead referenced for syntax when using NIX. 
+
+If you do end up using it, make sure to change the titus user folder and cloning my dwm-titus repo into your `~/GitHub/dwm-titus` folder from the overlay portion. This will build dwm every time `sudo nixos-rebuild switch` is run. 
+
+My hardware configuration is an example of NFS shares, LVM, and traditional partitions being mounted. Your Disk IDs and addresses will be different and it should NOT be used.
---
+++ configuration.nix
@@ -0,0 +1,249 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+                "openssl-1.1.1u"
+		"python-2.7.18.6"
+              ];
+hardware.opengl.driSupport32Bit = true;
+hardware.pulseaudio.support32Bit = true;
+nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem (builtins.parseDrvName pkg.name).name [ "steam" ]);
+nix.settings = {
+    substituters = ["https://nix-gaming.cachix.org"];
+    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+  };
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-studio"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    packages=[ pkgs.terminus_font ];
+    font="${pkgs.terminus_font}/share/consolefonts/ter-i22b.psf.gz";
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.windowManager.dwm.enable = true;
+  services.xserver.layout = "us";
+
+  services.xserver.displayManager = {
+	lightdm.enable = true;
+  	autoLogin = {
+		enable = true;
+		user = "titus";
+	};
+  };
+services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+'';
+  
+ services.picom.enable = true;
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.titus = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "kvm" "input" "disk" "libvirtd" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+        vim
+	wget
+	w3m
+	dmenu
+  neofetch
+	neovim
+	autojump
+	starship
+	brave
+	bspwm
+	cargo
+	celluloid
+	chatterino2
+  clang-tools_9
+	davinci-resolve
+	dwm
+	dunst
+	elinks
+	eww
+	feh
+	flameshot
+	flatpak
+  fontconfig
+  freetype
+	gcc
+	gh
+	gimp
+	git
+	github-desktop
+	gnugrep
+	gnumake
+	gparted
+	hugo
+	kitty
+	libverto
+  luarocks
+	lutris
+	mangohud
+	neovim
+	nfs-utils
+	ninja
+	nodejs
+	nomacs
+	openssl
+	pavucontrol
+	picom
+	polkit_gnome
+	powershell
+	protonup-ng
+	python3Full
+	python.pkgs.pip
+	qemu
+	ripgrep
+	rofi
+	steam
+	steam-run
+	sxhkd
+	st
+	stdenv
+	synergy
+	swaycons
+	terminus-nerdfont
+	tldr
+	trash-cli
+	unzip
+	variety
+	virt-manager
+	xclip
+	xdg-desktop-portal-gtk
+	xfce.thunar
+  xorg.libX11
+  xorg.libX11.dev
+  xorg.libxcb
+  xorg.libXft
+  xorg.libXinerama
+	xorg.xinit
+  xorg.xinput
+	(lutris.override {
+	       extraPkgs = pkgs: [
+		 # List package dependencies here
+		 wineWowPackages.stable
+		 winetricks
+	       ];
+	    })
+  ];
+
+  nixpkgs.overlays = [
+	(final: prev: {
+		dwm = prev.dwm.overrideAttrs (old: { src = /home/titus/GitHub/dwm-titus ;});
+	})
+  ];
+  
+  ## Gaming
+	programs.steam = {
+	  enable = true;
+	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	};
+	  
+
+  # List services that you want to enable:
+  virtualisation.libvirtd.enable = true; 
+  # enable flatpak support
+  services.flatpak.enable = true;
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    # wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+  security.polkit.enable = true;
+ systemd = {
+  user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+  };
+   extraConfig = ''
+     DefaultTimeoutStopSec=10s
+   '';
+}; 
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+  networking.enableIPv6 = false;
+
+fonts = {
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      font-awesome
+      source-han-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+      (nerdfonts.override { fonts = [ "Meslo" ]; })
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+	      monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+	      serif = [ "Noto Serif" "Source Han Serif" ];
+	      sansSerif = [ "Noto Sans" "Source Han Sans" ];
+      };
+    };
+};
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+  system.autoUpgrade.enable = true;  
+  system.autoUpgrade.allowReboot = true; 
+  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
+  
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
+
